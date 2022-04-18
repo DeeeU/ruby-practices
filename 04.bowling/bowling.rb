3628acac75d1be33840969353dbae2eeaa9aa816
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-04-18 10:43:59 +0900

    課題04　ボウリングのスコア計算プログラムの提出

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,18 +1,46 @@
-score = gets.split(',')
-round_number = 0
-round_score = 0
+scores = gets.split(',').freeze
+
+shots = []
+scores.each do |s|
+  if ['X', 'X\n', 'X \n'].include? s
+    shots << 10
+    shots << 0
+  else
+    shots << s.to_i
+  end
+end
+
+frames = []
+shots.each_slice(2) do |s|
+  frames << s
+end
+frames[-1] << 0
+
 total = 0
 
-score.size.times {|i|
-  if score[i] == "X"
-    total += 10
-    round_number = 2
-    round_score = "X"
+9.times do |i|
+  if i == 8 && frames[i][0] == 10
+    total += frames[i][0] + frames[i][1]
+    if frames[i + 1][0] != 10
+      total += frames[i + 1][0] + frames[i + 1][1]
+    else
+      total += frames[i + 1][0] + frames[i + 2][0]
+    end
+  elsif frames[i][0] == 10
+    total += frames[i][0] + frames[i][1]
+    if frames[i + 1][0] == 10
+      total += frames[i + 1][0] + frames[i + 2][0]
+    else
+      total += frames[i + 1][0] + frames[i + 1][1]
+    end
+
+  elsif frames[i][0] + frames[i][1] == 10
+    total += frames[i][0] + frames[i][1] + frames[i + 1][0]
+
   else
-    # p score[i].to_i
-    total += score[i].to_i
-    round_number += 1
-    # round_score += score[i].to_i
+    total += frames[i][0] + frames[i][1]
   end
-}
-puts total
+end
+
+total += frames[9..].flatten.inject(:+)
+p total
