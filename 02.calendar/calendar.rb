869367ba1af691ecf15d604a588d655a732b9ab9
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-05-09 10:38:12 +0900

    [fix]yearのnil回避

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,35 +1,38 @@
 require 'optparse'
 require 'Date'
 
-opt = ARGV.getopts("y:","m:")
+opt = ARGV.getopts('y:', 'm:')
 
 today = Date.today
-week = ["日","月","火","水","木","金","土"]
-year = opt["y"]
+week = %w[日 月 火 水 木 金 土]
+year = opt['y']
 
-if opt["m"] == nil
-  month = today.month.to_s
-else
-  month = opt["m"]
-end
+# 引数が無い際の処理
+month = if opt['m'].nil?
+          today.month.to_s
+        else
+          opt['m']
+        end
+
+year = if opt['y'].nil?
+         today.year.to_s
+       else
+         opt['y']
+       end
 
 first_week = Date.new(year.to_i, month.to_i, 1).wday
 number_day = Date.new(year.to_i, month.to_i, -1).day
 
-puts (month + "月　" + year).center(20)
-puts week.join(" ").center(10)
-print "   " * first_week
+puts (month + '月　' + year).center(20)
+puts week.join(' ').center(10)
+print '   ' * first_week
 
 num = first_week
 
-(1..number_day).each { |date|
-  print date.to_s.rjust(2) + " "
+(1..number_day).each do |date|
+  print date.to_s.rjust(2) + ' '
   num += 1
-  if num%7==0
-    print "\n"
-  end
-}
-
-if num % 7 != 0
-  print "\n"
+  print "\n" if num % 7 == 0
 end
+
+print "\n" if num % 7 != 0
