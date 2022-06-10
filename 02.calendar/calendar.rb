a8aa81e144c4658f108911edac3e3186b8e4b3ac
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-06-10 11:47:03 +0900

    [fix] to_iへの変更

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -10,17 +10,17 @@ week = %w[日 月 火 水 木 金 土]
 month = if opt['m'].nil?
           today.month
         else
-          opt['m']
+          opt['m'].to_i
         end
 
 year = if opt['y'].nil?
          today.year
        else
-         opt['y']
+         opt['y'].to_i
        end
 
-first_week = Date.new(year.to_i, month.to_i, 1).wday
-end_month = Date.new(year.to_i, month.to_i, -1).day
+first_week = Date.new(year, month, 1).wday
+end_month = Date.new(year, month, -1).day
 
 puts (month.to_s + '月　' + year.to_s).center(20)
 puts week.join(' ')
