25c84f08c888d31a0c6a5147b80cc1e8bebc0e19
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-11-25 10:13:45 +0900

    change : 例外処理の追加

--- 05.ls/ls_command.rb
+++ 05.ls/ls_command.rb
@@ -8,19 +8,24 @@ def how_much_space(arr)
   arr.max.length
 end
 
-files = Dir.glob('*')
-max_len = how_much_space(files)
-width = 3
-height = select_height(files, width)
-field = files.each_slice(height).to_a
 
-i = 0
-while i < hight
-  j = 0
-  while j < width
-    printf("%-#{max_len * 2}s", field[j][i])
-    j += 1
+begin
+  files = Dir.glob('*')
+  max_len = how_much_space(files)
+  width = 3
+  height = select_height(files, width)
+  field = files.each_slice(height).to_a
+
+  i = 0
+  while i < height
+    j = 0
+    while j < width
+      printf("%-#{max_len * 2}s", field[j][i])
+      j += 1
+    end
+    print("\n")
+    i += 1
   end
-  print("\n")
-  i += 1
+rescue => e
+  exit
 end
