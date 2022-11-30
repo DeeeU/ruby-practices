f164c5a76201d0cc85551a9279c9620a26927804
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2022-11-30 15:35:58 +0900

    change: 例外処理の撤廃とwidthの再定義

--- 05.ls/ls_command.rb
+++ 05.ls/ls_command.rb
@@ -4,23 +4,27 @@ def height(files_data, width)
   (files_data.length / width.to_f).ceil
 end
 
+def width(files_data, width)
+  if files_data.length < width
+    return files_data.length
+  else
+    return width
+  end
+end
+
 def space(files_data)
   files_data.max_by { |a, b| a.to_f <=> b.to_f }.length
 end
 
-begin
-  files = Dir.glob('*')
-  max_length = space(files)
-  width = 3
-  height = height(files, width)
-  field = files.each_slice(height).to_a
+files = Dir.glob('*/*')
+max_length = space(files)
+width = width(files, 3)
+height = height(files, width)
+field = files.each_slice(height).to_a
 
-  height.times do |i|
-    width.times do |j|
-      printf("%-#{max_length * 2}s", field[j][i])
-    end
-    print("\n")
+height.times do |i|
+  width.times do |j|
+    printf("%-#{max_length * 2}s", field[j][i])
   end
-rescue NoMethodError
-  exit
+  print("\n")
 end
