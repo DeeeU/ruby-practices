8c7e5386ab012eb4ff7b8a24a0f744364f5f37e8
Y.Kanabuchi <yutaro.kanabuchi@medpeer.co.jp>
2023-02-20 15:42:13 +0900

    change : 各列の最長文字数による出力の幅の決定

--- 05.ls/ls_command.rb
+++ 05.ls/ls_command.rb
@@ -62,6 +62,14 @@ def max_length(files_data)
   files_data.max_by(&:length).length
 end
 
+def show_file(files_data, column_number)
+  max_number = 0
+  files_data.each do |i|
+    max_number = i[column_number].length if max_number < i[column_number].length
+  end
+  max_number
+end
+
 params = ARGV.getopts('a', 'r', 'l')
 files = if params['a']
           Dir.foreach('.').to_a.sort
@@ -80,12 +88,12 @@ if params['l']
     fs = File::Stat.new(files[i])
     sum_block += fs.blocks
     file_info[i] << create_permission(fs.mode.to_s(8).rjust(6, '0'))
-    file_info[i] << File::Stat.new(files[i]).nlink
+    file_info[i] << File::Stat.new(files[i]).nlink.to_s
     file_info[i] << Etc.getpwuid(fs.uid).name
     file_info[i] << Etc.getgrgid(fs.gid).name
-    file_info[i] << File::Stat.new(files[i]).size
-    file_info[i] << File::Stat.new(files[i]).mtime.month
-    file_info[i] << File::Stat.new(files[i]).mtime.day
+    file_info[i] << File::Stat.new(files[i]).size.to_s
+    file_info[i] << File::Stat.new(files[i]).mtime.month.to_s
+    file_info[i] << File::Stat.new(files[i]).mtime.day.to_s
     file_info[i] << "#{File::Stat.new(files[i]).mtime.hour}:#{File::Stat.new(files[i]).mtime.min.to_s.rjust(2, '0')}"
     file_info[i] << files[i]
   end
@@ -93,8 +101,8 @@ if params['l']
   printf 'total %d', sum_block
   print("\n")
   file_info.each do |i|
-    i.each do |j|
-      printf("%-#{max_length}s ", j)
+    i.length.times do |j|
+      printf(' %s', i[j].rjust(show_file(file_info, j)))
     end
     print("\n")
   end
